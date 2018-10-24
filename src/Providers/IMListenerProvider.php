017a90ec02a32d67303e280841ffaa2c7913fe85
chenyang <devchen.org@gmail.com>
2018-10-25 00:10:16 +0800

    use Listener Providers

---
+++ src/Providers/IMListenerProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace DevChen\SwooleIM\Providers;
+
+use League\Event\ListenerAcceptorInterface;
+use League\Event\ListenerProviderInterface;
+
+class IMListenerProvider implements ListenerProviderInterface
+{
+    public function provideListeners(ListenerAcceptorInterface $listenerAcceptor)
+    {
+        
+    }
+
+}
